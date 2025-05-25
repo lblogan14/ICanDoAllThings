3723a482794f8d974ff48433b02552f92e554dd2
Bin Liu <binliu14qd@gmail.com>
2025-05-25 09:48:17 -0500

    SwiftUI Day 21

---
+++ 100DaysOfSwiftUI/Project-02/Day21/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Bin Liu on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

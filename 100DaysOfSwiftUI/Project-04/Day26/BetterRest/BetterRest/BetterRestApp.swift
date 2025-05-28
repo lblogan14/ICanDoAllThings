51b987245e531ad49bc0e9cdc925a1b35bbc7c2b
Bin Liu <binliu14qd@gmail.com>
2025-05-28 09:33:32 -0500

    SwiftUI Day 26

---
+++ 100DaysOfSwiftUI/Project-04/Day26/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Bin Liu on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

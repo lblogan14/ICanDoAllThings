a8ad28ad321966326d7d3f1deba29c3f125331a0
Bin Liu <binliu14qd@gmail.com>
2025-06-21 13:28:21 -0500

    Day 41 main code

---
+++ 100DaysOfSwiftUI/Project-08/Day41/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Bin Liu on 6/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+    let columns = [
+            GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            //Text("Detail view")
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                    // previous broken text view
+                                    //Text(mission.launchDate ?? "N/A")
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}

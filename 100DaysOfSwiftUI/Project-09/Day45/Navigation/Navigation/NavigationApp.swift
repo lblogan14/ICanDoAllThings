158ac7a3a9014de4482ecf5f9f976dc383df2e57
Bin Liu <binliu14qd@gmail.com>
2025-06-28 09:21:33 -0500

    SwiftUI Day 45

---
+++ 100DaysOfSwiftUI/Project-09/Day45/Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Bin Liu on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

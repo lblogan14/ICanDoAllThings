0f111a2463baec3fd533c0738b71f78c57f74b8f
Bin Liu <binliu14qd@gmail.com>
2025-10-30 14:51:10 -0500

    tutorial 5 for fastapi

--- FastAPI/src/app/main.py
+++ FastAPI/src/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
+from typing import Any
 
 app = FastAPI()
 
@@ -10,14 +11,25 @@ def get_shipment():
         'status': 'in transit',
     }
 
+@app.get("/shipment/latest")
+def get_latest_shipment():
+    return {
+        'id': 101,
+        'weight': 15.0,
+        'content': 'glassware',
+        'status': 'delivered',
+    }
 
 @app.get("/shipment/{id}")
-def get_shipment_id(id: int) -> dict[str, str | int]:
+def get_shipment_id(id: int) -> dict[str, Any]:
     return {
         'id': id,
+        'weight': 12.5,
         'content': 'wooden table',
         'status': 'in transit',
     }
+
+
 
 @app.get("/scalar", include_in_schema=False)
 def get_scalar_docs():

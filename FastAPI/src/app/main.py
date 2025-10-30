a2a8c43c7998dfe31d85808d4db31ddfb87f3747
Bin Liu <binliu14qd@gmail.com>
2025-10-30 09:49:26 -0500

    tutorial 3 for fastapi

--- FastAPI/src/app/main.py
+++ FastAPI/src/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 
 app = FastAPI()
 
@@ -8,3 +9,10 @@ def get_shipment():
         'content': 'wooden table',
         'status': 'in transit',
     }
+
+@app.get("/scalar", include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API"
+    )

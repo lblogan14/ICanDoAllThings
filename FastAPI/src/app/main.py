5664afaf06f4f59b133d73567727baed7943b1f7
Bin Liu <binliu14qd@gmail.com>
2025-11-04 14:24:12 -0600

    tutorial 6 for fastapi

--- FastAPI/src/app/main.py
+++ FastAPI/src/app/main.py
@@ -4,6 +4,39 @@ from typing import Any
 
 app = FastAPI()
 
+shipments = {
+    12701: {
+        'weight': 15.0,
+        'content': 'glassware',
+        'status': 'delivered',
+    },
+    12702: {
+        'weight': 5.5,
+        'content': 'books',
+        'status': 'in transit',
+    },
+    12703: {
+        'weight': 2.3,
+        'content': 'clothes',
+        'status': 'pending',
+    },
+    12704: {
+        'weight': 7.8,
+        'content': 'electronics',
+        'status': 'delivered',
+    },
+    12705: {
+        'weight': 3.4,
+        'content': 'toys',
+        'status': 'in transit',
+    },
+    12706: {
+        'weight': 12.0,
+        'content': 'furniture',
+        'status': 'pending',
+    }
+}
+
 @app.get("/shipment")
 def get_shipment():
     return {
@@ -11,23 +44,21 @@ def get_shipment():
         'status': 'in transit',
     }
 
+
 @app.get("/shipment/latest")
 def get_latest_shipment():
-    return {
-        'id': 101,
-        'weight': 15.0,
-        'content': 'glassware',
-        'status': 'delivered',
-    }
+    id = max(shipments.keys())
+    return shipments[id]
+
 
 @app.get("/shipment/{id}")
 def get_shipment_id(id: int) -> dict[str, Any]:
-    return {
-        'id': id,
-        'weight': 12.5,
-        'content': 'wooden table',
-        'status': 'in transit',
-    }
+    if id not in shipments:
+        return {
+            "detail": "Given ID does not exist!"
+        }
+
+    return shipments[id]
 
 
 
